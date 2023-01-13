394a2315cb5159b28adc5e4d42a918a2befe8dd4
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2023-01-12 23:13:42 -0400

    colocando tipado a las peticiones http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,7 @@
+import { Gif } from './../interface/gifs.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +12,7 @@ export class GifsService {
 
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -25,10 +27,10 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
     this.http
-      .get(
+      .get<SearchGifsResponse>(
         `https://api.giphy.com/v1/gifs/search?api_key=9JXEpXIn92Eqj215o9NGzPCBE9Kwf9TA&q=${query}&limit=10`
       )
-      .subscribe((res: any) => {
+      .subscribe((res) => {
         console.log(res.data);
         this.resultados = res.data;
       });
